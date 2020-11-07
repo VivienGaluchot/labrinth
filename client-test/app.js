a1c2e28f77a45daa1533cfb308360917ca5fc116
Vivien Galuchot <vivien.galuchot@gmail.com>
2020-11-07 12:46:04 +0100

    wip

--- client-test/app.js
+++ client-test/app.js
@@ -1,11 +1,145 @@
 
+const directions = {
+    TOP: 'top',
+    RIGHT: 'right',
+    BOTTOM: 'bottom',
+    LEFT: 'left'
+}
+
+// game
+
+class Piece {
+    constructor() {
+        this.paths = {
+            [directions.TOP]: false,
+            [directions.RIGHT]: false,
+            [directions.BOTTOM]: false,
+            [directions.LEFT]: false
+        }
+    }
+
+    isOpen(direction) {
+        return this.paths[direction];
+    }
+
+    rotateClockwise() {
+        let top = this.paths[directions.TOP];
+        this.paths[directions.TOP] = this.paths[directions.LEFT];
+        this.paths[directions.LEFT] = this.paths[directions.BOTTOM];
+        this.paths[directions.BOTTOM] = this.paths[directions.RIGHT];
+        this.paths[directions.RIGHT] = top;
+    }
+
+    draw(ctx, x, y, w, h) {
+        let run = (exec) => {
+            let wArray = [w / 3, w / 3, w / 3];
+            let xOffset = [0, wArray[0], wArray[0] + wArray[1]];
+            let hArray = [h / 3, h / 3, h / 3];
+            let yOffset = [0, hArray[0], hArray[0] + hArray[1]];
+            for (let i of [0, 1, 2]) {
+                let wi = wArray[i];
+                let xi = x + xOffset[i];
+                for (let j of [0, 1, 2]) {
+                    let hj = hArray[j];
+                    let yj = y + yOffset[j];
+                    let isOpen = true;
+                    if ((i == 0 || i == 2) && (j == 0 || j == 2)) {
+                        isOpen = false;
+                    } else if (i == 1 && j == 0 && !this.isOpen(directions.TOP)) {
+                        isOpen = false;
+                    } else if (i == 0 && j == 1 && !this.isOpen(directions.LEFT)) {
+                        isOpen = false;
+                    } else if (i == 1 && j == 2 && !this.isOpen(directions.BOTTOM)) {
+                        isOpen = false;
+                    } else if (i == 2 && j == 1 && !this.isOpen(directions.RIGHT)) {
+                        isOpen = false;
+                    }
+                    exec(isOpen, xi, yj, wi, hj);
+                }
+            }
+        };
+
+        ctx.save();
+        run((isOpen, x, y, w, h) => {
+            if (isOpen) {
+                ctx.fillStyle = "#FFF";
+            } else {
+                ctx.fillStyle = "#888";
+            }
+            ctx.fillRect(x, y, Math.ceil(w), Math.ceil(h));
+        });
+        run((isOpen, x, y, w, h) => {
+            ctx.strokeStyle = "#444";
+            ctx.strokeRect(x, y, Math.ceil(w), Math.ceil(h));
+        });
+        ctx.restore();
+    }
+}
+
+class PieceL extends Piece {
+    constructor() {
+        super();
+        this.paths = {
+            [directions.TOP]: true,
+            [directions.RIGHT]: true,
+            [directions.BOTTOM]: false,
+            [directions.LEFT]: false
+        }
+    }
+}
+
+class PieceI extends Piece {
+    constructor() {
+        super();
+        this.paths = {
+            [directions.TOP]: true,
+            [directions.RIGHT]: false,
+            [directions.BOTTOM]: true,
+            [directions.LEFT]: false
+        }
+    }
+}
+
+class PieceT extends Piece {
+    constructor() {
+        super();
+        this.paths = {
+            [directions.TOP]: true,
+            [directions.RIGHT]: true,
+            [directions.BOTTOM]: false,
+            [directions.LEFT]: true
+        }
+    }
+}
+
+class PieceX extends Piece {
+    constructor() {
+        super();
+        this.paths = {
+            [directions.TOP]: true,
+            [directions.RIGHT]: true,
+            [directions.BOTTOM]: true,
+            [directions.LEFT]: true
+        }
+    }
+}
+
+let pieceL = new PieceL();
+let pieceI = new PieceI();
+let pieceT = new PieceT();
+let pieceX = new PieceX();
+
+
+// canvas
+
 let canvas = document.getElementById("main-canvas");
 
-function countour(canvas) {
+function draw(canvas) {
     let width = canvas.width;
     let height = canvas.height;
 
     let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, width, height);
 
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#000000";
@@ -17,10 +151,27 @@ function countour(canvas) {
     ctx.lineTo(width, 0);
     ctx.lineTo(0, 0);
     ctx.stroke();
+
+    let s = 60;
+    pieceL.draw(ctx, (width - s) / 2 - s, (height - s) / 2, s, s);
+    pieceI.draw(ctx, (width - s) / 2, (height - s) / 2, s, s);
+    pieceT.draw(ctx, (width - s) / 2 + s, (height - s) / 2, s, s);
+    pieceX.draw(ctx, (width - s) / 2, (height - s) / 2 + s, s, s);
 }
 
-countour(canvas);
+draw(canvas);
 
+setInterval(() => {
+    pieceL.rotateClockwise();
+    pieceI.rotateClockwise();
+    pieceT.rotateClockwise();
+    pieceX.rotateClockwise();
+    draw(canvas);
+}, 1000);
+
+
+
+// actions
 
 const opCount = 4;
 

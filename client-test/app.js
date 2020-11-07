8d0b57435dacc5f88121d0a36e23c0774499b25c
Vivien Galuchot <vivien.galuchot@gmail.com>
2020-11-07 18:33:13 +0100

    functional proto

--- client-test/app.js
+++ client-test/app.js
@@ -6,6 +6,26 @@ const directions = {
     LEFT: 'left'
 }
 
+function reverse(direction) {
+    switch (direction) {
+        case directions.TOP:
+            return directions.BOTTOM;
+        case directions.BOTTOM:
+            return directions.TOP;
+        case directions.RIGHT:
+            return directions.LEFT;
+        case directions.LEFT:
+            return directions.RIGHT;
+        default:
+            throw Error("wrong direction");
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
 // game
 
 class Piece {
@@ -30,18 +50,22 @@ class Piece {
         this.paths[directions.RIGHT] = top;
     }
 
+    rotateAntiClockwise() {
+        let top = this.paths[directions.TOP];
+        this.paths[directions.TOP] = this.paths[directions.RIGHT];
+        this.paths[directions.RIGHT] = this.paths[directions.BOTTOM];
+        this.paths[directions.BOTTOM] = this.paths[directions.LEFT];
+        this.paths[directions.LEFT] = top;
+    }
+
     draw(ctx, x, y, w, h) {
         let run = (exec) => {
-            let wArray = [w / 3, w / 3, w / 3];
-            let xOffset = [0, wArray[0], wArray[0] + wArray[1]];
-            let hArray = [h / 3, h / 3, h / 3];
-            let yOffset = [0, hArray[0], hArray[0] + hArray[1]];
             for (let i of [0, 1, 2]) {
-                let wi = wArray[i];
-                let xi = x + xOffset[i];
+                let wi = w / 3;
+                let xi = x + i * wi;
                 for (let j of [0, 1, 2]) {
-                    let hj = hArray[j];
-                    let yj = y + yOffset[j];
+                    let hj = h / 3;
+                    let yj = y + j * hj;
                     let isOpen = true;
                     if ((i == 0 || i == 2) && (j == 0 || j == 2)) {
                         isOpen = false;
@@ -64,14 +88,25 @@ class Piece {
             if (isOpen) {
                 ctx.fillStyle = "#FFF";
             } else {
-                ctx.fillStyle = "#888";
+                ctx.fillStyle = "#555";
             }
             ctx.fillRect(x, y, Math.ceil(w), Math.ceil(h));
+            if (!isOpen) {
+                let border = 4;
+                ctx.lineWidth = border;
+                ctx.strokeStyle = "#0001";
+                ctx.strokeRect(x + border / 2, y + border / 2, Math.ceil(w) - border, Math.ceil(h) - border);
+            }
         });
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "#0002";
         run((isOpen, x, y, w, h) => {
-            ctx.strokeStyle = "#444";
             ctx.strokeRect(x, y, Math.ceil(w), Math.ceil(h));
         });
+        let border = 4;
+        ctx.lineWidth = border;
+        ctx.strokeStyle = "#0002";
+        ctx.strokeRect(x + border / 2, y + border / 2, w - border, h - border);
         ctx.restore();
     }
 }
@@ -124,10 +159,188 @@ class PieceX extends Piece {
     }
 }
 
-let pieceL = new PieceL();
-let pieceI = new PieceI();
-let pieceT = new PieceT();
-let pieceX = new PieceX();
+class Pion {
+    constructor(name, color, startCol) {
+        this.name = name;
+        this.color = color;
+        this.startCol = startCol;
+    }
+
+    draw(ctx, x, y, w, h) {
+        ctx.save();
+        ctx.strokeStyle = `#${this.color}FF`;
+        ctx.fillStyle = `#${this.color}44`;
+        ctx.lineWidth = 4;
+
+        ctx.beginPath();
+        let r = Math.min(h / 2, w / 2) / 4;
+        ctx.arc(x + w / 2, y + h / 2, r, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+
+        ctx.fillStyle = `#${this.color}`;
+        ctx.font = `${Math.round(r)}px arial`;
+        ctx.fillText(this.name, x + w / 2 - r / 3, y + h / 2 + r / 3);
+        ctx.restore();
+    }
+}
+
+class Grid {
+    constructor(rowCount, colCount) {
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+
+        this.rows = [];
+
+        this.pions = new Map();
+
+        let getRandomPiece = () => {
+            let x = Math.random();
+            let p = null;
+            if (x < 0.3) {
+                p = new PieceI();
+            } else if (x < 0.6) {
+                p = new PieceL();
+            } else if (x < 0.8) {
+                p = new PieceT();
+            } else {
+                p = new PieceX();
+            }
+            let angle = getRandomInt(4);
+            for (let i = 0; i < angle; i++) {
+                p.rotateClockwise();
+            }
+            return p;
+        }
+
+        for (let r = 0; r < rowCount; r++) {
+            let col = [];
+            for (let c = 0; c < colCount; c++) {
+                col.push(getRandomPiece());
+            }
+            this.rows.push(col);
+        }
+    }
+
+    getPiece(row, col) {
+        return this.rows[row][col];
+    }
+
+    getNextPos(row, col, direction) {
+        let next = { "row": row, "col": col };
+        switch (direction) {
+            case directions.TOP:
+                next.row += 1;
+                break;
+            case directions.BOTTOM:
+                next.row -= 1;
+                break;
+            case directions.RIGHT:
+                next.col += 1;
+                break;
+            case directions.LEFT:
+                next.col -= 1;
+                break;
+            default:
+                throw Error("wrong direction");
+        }
+        if (next.row < 0 || next.row >= this.rowCount ||
+            next.col < 0 || next.col >= this.colCount) {
+            return null;
+        }
+        return next;
+    }
+
+    draw(ctx, x, y, w, h) {
+        let wCol = w / this.colCount;
+        let hRow = h / this.rowCount;
+        let sPiece = Math.min(wCol, hRow);
+        let trueW = sPiece * this.colCount;
+        let trueH = sPiece * this.rowCount;
+        let paddingW = (w - trueW) / 2;
+        let paddingH = (h - trueH) / 2;
+
+        let spacing = 2;
+        sPiece -= spacing;
+        paddingW += spacing / 2;
+        paddingH += spacing / 2;
+
+        for (let i in this.rows) {
+            let py = h - (paddingH + y + i * (sPiece + spacing) + sPiece);
+            for (let j in this.rows[i]) {
+                let px = paddingW + x + j * (sPiece + spacing);
+                this.getPiece(i, j).draw(ctx, px, py, sPiece, sPiece);
+                for (const [pion, pos] of this.pions) {
+                    if (i == pos.row && j == pos.col) {
+                        pion.draw(ctx, px, py, sPiece, sPiece);
+                    }
+                }
+            }
+        }
+    }
+
+    addPion(pion) {
+        this.pions.set(pion, { "row": 0, "col": pion.startCol });
+    }
+
+    move(pion, direction) {
+        let startPos = this.pions.get(pion);
+        let nextPos = this.getNextPos(startPos.row, startPos.col, direction);
+
+        let start = this.getPiece(startPos.row, startPos.col);
+        let next = nextPos ? this.getPiece(nextPos.row, nextPos.col) : null;
+
+        if (next != null && start.isOpen(direction) && next.isOpen(reverse(direction))) {
+            this.pions.set(pion, nextPos);
+        }
+    }
+
+    shift(pion, direction) {
+        let startPos = this.pions.get(pion);
+        let row = this.rows[startPos.row];
+        if (direction == directions.LEFT) {
+            let first = row[0];
+            row = row.slice(1, row.length);
+            row.push(first);
+            for (let [pion, pos] of this.pions) {
+                if (pos.row == startPos.row) {
+                    pos.col = pos.col == 0 ? this.colCount - 1 : pos.col - 1;
+                    this.pions.set(pion, pos);
+                }
+            }
+        } else if (direction == directions.RIGHT) {
+            let last = row.pop();
+            row.unshift(last);
+            for (let [pion, pos] of this.pions) {
+                if (pos.row == startPos.row) {
+                    pos.col = (pos.col + 1) % this.colCount;
+                    this.pions.set(pion, pos);
+                }
+            }
+        }
+        this.rows[startPos.row] = row;
+    }
+
+    rotate(pion, isClockwise) {
+        let startPos = this.pions.get(pion);
+        let row = this.rows[startPos.row];
+        for (let piece of row) {
+            if (isClockwise) {
+                piece.rotateClockwise();
+            } else {
+                piece.rotateAntiClockwise();
+            }
+        }
+    }
+}
+
+
+let grid = new Grid(7, 5);
+let pionA = new Pion("A", "AA00AA", 1);
+let pionB = new Pion("B", "00AAAA", 3);
+
+grid.addPion(pionA);
+grid.addPion(pionB);
 
 
 // canvas
@@ -152,28 +365,17 @@ function draw(canvas) {
     ctx.lineTo(0, 0);
     ctx.stroke();
 
-    let s = 60;
-    pieceL.draw(ctx, (width - s) / 2 - s, (height - s) / 2, s, s);
-    pieceI.draw(ctx, (width - s) / 2, (height - s) / 2, s, s);
-    pieceT.draw(ctx, (width - s) / 2 + s, (height - s) / 2, s, s);
-    pieceX.draw(ctx, (width - s) / 2, (height - s) / 2 + s, s, s);
+    grid.draw(ctx, 0, 0, width, height);
 }
 
 draw(canvas);
-
-setInterval(() => {
-    pieceL.rotateClockwise();
-    pieceI.rotateClockwise();
-    pieceT.rotateClockwise();
-    pieceX.rotateClockwise();
-    draw(canvas);
-}, 1000);
-
 
 
 // actions
 
 const opCount = 4;
+
+let turnNb = 0;
 
 const playersFifo = {
     "fifo-a": [],
@@ -187,11 +389,66 @@ const opIcons = {
     "right": "fas fa-arrow-right",
     "shift-left": "fas fa-angle-double-left",
     "shift-right": "fas fa-angle-double-right",
+    "rotate-clock": "fas fa-undo mirror",
+    "rotate-anti_clock": "fas fa-undo",
     "reset": "far fa-trash-alt"
 };
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 let tourBtn = document.getElementById("tour-btn");
-tourBtn.onclick = () => {
+tourBtn.onclick = async () => {
+    turnNb++;
+
+    let runOp = (op, pion) => {
+        switch (op) {
+            case "left":
+                grid.move(pion, directions.LEFT);
+                break;
+            case "up":
+                grid.move(pion, directions.TOP);
+                break;
+            case "down":
+                grid.move(pion, directions.BOTTOM);
+                break;
+            case "right":
+                grid.move(pion, directions.RIGHT);
+                break;
+            case "shift-left":
+                grid.shift(pion, directions.LEFT);
+                break;
+            case "shift-right":
+                grid.shift(pion, directions.RIGHT);
+                break;
+            case "rotate-clock":
+                grid.rotate(pion, true);
+                break;
+            case "rotate-anti_clock":
+                grid.rotate(pion, false);
+                break;
+        }
+        draw(canvas);
+    };
+
+    for (let i = 0; i < opCount; i++) {
+        let entryA = playersFifo["fifo-a"][i];
+        let entryB = playersFifo["fifo-b"][i];
+
+        entryA.setActive(true);
+        await sleep(200);
+        runOp(entryA.op, pionA);
+        await sleep(1000);
+        entryA.setActive(false);
+
+        entryB.setActive(true);
+        await sleep(200);
+        runOp(entryB.op, pionB);
+        await sleep(1000);
+        entryB.setActive(false);
+    }
+
     for (let slot of playersFifo["fifo-a"]) {
         slot.reset();
     }
@@ -223,8 +480,18 @@ function registerAction(btn) {
                     slot.innerHTML = `<i class="${opIcons[op]}"></i>`;
                     playersFifo[target].push({
                         "op": op,
+                        "setActive": (isActive) => {
+                            if (isActive) {
+                                slot.classList.add("active");
+                            } else {
+                                slot.classList.remove("active");
+                                slot.classList.add("used");
+                            }
+                        },
                         "reset": () => {
                             slot.classList.add("empty");
+                            slot.classList.remove("active");
+                            slot.classList.remove("used");
                             slot.textContent = "";
                         }
                     });

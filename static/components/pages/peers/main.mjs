ac087facc1bb0e8ef26a7730c933839ce0be541b
Vivien Galuchot <vivien.galuchot@gmail.com>
2021-01-28 09:02:03 +0100

    rework pages

---
+++ static/components/pages/peers/main.mjs
@@ -0,0 +1,20 @@
+"use strict";
+
+class Component {
+    // called when the component is instantiated
+    constructor(element) {
+        this.element = element;
+    }
+
+    // called when the component is rendered
+    onRender() {
+
+    }
+
+    // called when the component is removed
+    onRemove() {
+
+    }
+}
+
+export { Component }

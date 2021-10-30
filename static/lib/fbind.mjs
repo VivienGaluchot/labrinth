b2694bc3485b032a063e08708157e39c19cd44e7
Vivien Galuchot <vivien.galuchot@gmail.com>
2021-10-30 12:09:37 +0200

    proxy binding wip

--- static/lib/fbind.mjs
+++ static/lib/fbind.mjs
@@ -5,7 +5,7 @@
 "use strict";
 
 import * as MinComponent from '/lib/min-component.mjs';
-import { FNode } from '/lib/fdom.mjs';
+import * as FDom from '/lib/fdom.mjs';
 
 
 let bindCounter = 0;
@@ -21,7 +21,7 @@ class FBinderAtomic {
     }
 
     bind(element, render) {
-        if (element instanceof FNode) {
+        if (element instanceof FDom.FNode) {
             element = element.element;
         }
         if (!this.renders.has(render)) {
@@ -69,6 +69,184 @@ class FBinderObject {
             }
         }
     }
+}
+
+
+/**
+ * Proxy binding
+ */
+
+function proxAny(target, onchange) {
+    if (target instanceof Array) {
+        return proxArr(target, onchange);
+    } else if (target instanceof Object) {
+        return proxObj(target, onchange);
+    } else {
+        return target;
+    }
+}
+
+function proxObj(target, onchange) {
+    for (let prop in target) {
+        target[prop] = proxAny(target[prop], onchange);
+    }
+    return new Proxy(target, {
+        set: (obj, prop, value) => {
+            console.log("object prop", prop, "set to", value);
+            obj[prop] = proxAny(value, onchange);
+            onchange();
+            return true;
+        }
+    });
+}
+
+function proxArr(target, onchange) {
+    for (let prop in target) {
+        target[prop] = proxAny(target[prop], onchange);
+    }
+    return new Proxy(target, {
+        set: (target, prop, value) => {
+            // called when an array index is set
+            console.log("array prop", prop, "set to", value);
+            target[prop] = proxAny(value, onchange);
+            onchange();
+            return true;
+        },
+        get(target, prop) {
+            // do not call onchange for each intermediate change made by push, unshift, pop
+            const val = target[prop];
+            if (typeof val === 'function') {
+                if (['push', 'unshift', 'pop', 'fill', 'sort'].includes(prop)) {
+                    return function () {
+                        console.log('array operation', prop, arguments);
+                        let ret = Array.prototype[prop].apply(target, arguments);
+                        onchange();
+                        return ret;
+                    }
+                } else {
+                    return val.bind(target);
+                }
+            } else {
+                return val;
+            }
+        }
+    });
+}
+
+
+try {
+    class Watch {
+        constructor(val) {
+            this.renders = [];
+            this.proxy = null;
+            this.rerender = () => {
+                for (let render of this.renders) {
+                    render(this.proxy);
+                }
+            };
+            this.proxy = proxAny(val, this.rerender);
+        }
+
+        set object(val) {
+            console.log("set root object", val);
+            this.proxy = proxAny(val, this.rerender);
+            this.rerender();
+        }
+
+        get object() {
+            return this.proxy;
+        }
+
+        bind(render) {
+            this.renders.push(render);
+            render(this.object);
+        }
+    }
+
+    let watch = new Watch({
+        a: 1,
+        b: 1,
+        c: [1],
+        d: {
+            a: 1,
+            b: {
+                a: [1]
+            }
+        }
+    });
+    watch.bind((object) => {
+        let x = `I) a = ${object.a}, b = ${object.b}, c = ${object.c}, d.a = ${object.d.a}, d.b.a = ${object.d.b.a}`;
+        console.log(x);
+    });
+    // set property
+    console.log("-");
+    watch.object.a++;
+    console.log("-");
+    watch.object.b++;
+
+    // add array value
+    console.log("-");
+    // TODO check why double render here
+    watch.object.c.push(1);
+    console.log("-");
+    watch.object.c.push(1);
+
+    // set array value
+    console.log("-");
+    watch.object.c[0] = 2;
+    console.log("-");
+    watch.object.c = [3, 3, 3,];
+
+    // set object value
+    console.log("-");
+    watch.object.d.a = 2;
+    console.log("-");
+    watch.object.d.b.a[0] = 2;
+
+    // set an object
+    console.log("-");
+    watch.object.d = {
+        a: 1,
+        b: {
+            a: [1]
+        }
+    };
+
+    console.log("------------");
+    let watch_2 = new Watch({
+        a: 1,
+        b: 1
+    });
+    watch_2.bind((object) => {
+        let x = `I) a = ${object.a}, b = ${object.b}`;
+        console.log(x);
+    });
+    watch_2.bind((object) => {
+        let x = `II) a = ${object.a}, b = ${object.b}`;
+        console.log(x);
+    });
+    watch_2.object.a++;
+    watch_2.object.b++;
+
+    console.log("------------");
+    let watch_3 = new Watch([1, 2, 3]);
+    watch_3.bind((object) => {
+        let x = `I) ${object}`;
+        console.log(x);
+    });
+
+    watch_3.object.push(4);
+    watch_3.object.pop();
+    watch_3.object.unshift(0);
+    watch_3.object[0] = 25;
+    watch_3.object.sort((a, b) => { return a - b });
+    watch_3.object.fill(0, 2, 4);
+
+    watch_3.object = [1, 2, 3];
+    watch_3.object.push(4);
+
+} catch (err) {
+    console.error(err);
 }
 
 
